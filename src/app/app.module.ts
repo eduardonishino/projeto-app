87503b8a2bcf807f4ba1e637bcd40831dd1ccf8e
eduardonishino <eduardo_nishino@hotmail.com>
2020-09-08 16:52:43 -0300

    implementação de componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './pages/login-page.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'login', component:LoginPageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
